948d4ee18e1da72c0a8ca20fb34245200d8e6889
mnkhmtv <diminnakh2209@gmail.com>
2022-01-18 22:39:26 +0300

    buttons

--- mathhelp.py
+++ mathhelp.py
@@ -3,6 +3,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.types import InputFile
 
+import logging
 
 from config import TOKEN
 
@@ -12,70 +13,108 @@ from os.path import isfile, join
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
 
 @dp.message_handler(commands=["start"])
 async def process_start_command(message: types.Message):
     await message.reply(
-        "Привет!\nПривет! Меня зовут MathHelp и я могу помочь тебе с подготовкой к ЕГЭ по математике!\nНажми /go, чтобы начать или /help - он поможет тебе освоиться здесь "
+        "Привет! Меня зовут MathHelp и я могу помочь тебе с подготовкой к ЕГЭ по математике!\nНажми /go, чтобы начать"
+    )
+    #list_of_dirs: list = list()
+    #mypath = "./maths/"
+    #for dirname in os.listdir(mypath):
+    #    names = dirname.split("-")
+    #    list_of_dirs.append((names[1], "folder:" + names[0]))
+
+    #keyboard_markup = types.InlineKeyboardMarkup(row_width=1)
+    #row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in list_of_dirs)
+    #keyboard_markup.add(*row_btns)
+    #await message.reply('Нажми go, чтобы начать или help - он поможет тебе освоиться здесь', reply_markup=keyboard_markup)
+
+
+@dp.message_handler(commands='go')
+async def process_start_command(message: types.Message):
+    await message.reply(
+            "Для того, чтобы получить файл со всеми формулами жми /formula. Для того, чтобы решить задачи/примеры жми /task"
     )
 
+
+
+@dp.message_handler(commands='formula')
+async def process_start_command(message: types.Message):
     list_of_dirs: list = list()
-    mypath = "./maths/"
+    mypath = "./формула/"
     for dirname in os.listdir(mypath):
         names = dirname.split("-")
-        list_of_dirs.append(('/' + names[1], "folder:" + names[0]))
+        list_of_dirs.append((names[1], "folder:" + names[0]))
 
     keyboard_markup = types.InlineKeyboardMarkup(row_width=1)
     row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in list_of_dirs)
     keyboard_markup.add(*row_btns)
-    await message.reply('', reply_markup=keyboard_markup)
+    await message.reply('Основные формулы', reply_markup=keyboard_markup)
 
 
-@dp.callback_query_handler()
-async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
-    buttons: list = list()
-    answer_data = query.data
-    await query.answer(f"You answered with {answer_data!r}")
+@dp.message_handler(commands='task')
+async def process_start_command(message: types.Message):
+    list_of_dirs: list = list()
+    mypath = "./задания/"
+    for dirname in os.listdir(mypath):
+        names = dirname.split(".")
+        list_of_dirs.append((names[1], "folder:" + names[0]))
+
     keyboard_markup = types.InlineKeyboardMarkup(row_width=1)
-    print(f"{answer_data=}")
+    row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in list_of_dirs)
+    keyboard_markup.add(*row_btns)
+    await message.reply('Выберите нужную тему', reply_markup=keyboard_markup)
 
-    _tpath = answer_data.split(":")
+#@dp.callback_query_handler()
+#async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
+#    buttons: list = list()
+#    answer_data = query.data
+#    await query.answer(f"You answered with {answer_data!r}")
+#    keyboard_markup = types.InlineKeyboardMarkup(row_width=1)
+#    print(f"{answer_data=}")
 
-    if _tpath[0] == "folder":
-        paths = _deocde_to_paths(answer_data)
-        dir_name = "/".join(paths)
-        tg_path = [x.split("-")[0] for x in paths[1:]]
+#    _tpath = answer_data.split(":")
 
-        buttons = _get_listdir(dir_name, tg_path)
+#    if _tpath[0] == "folder":
+#        paths = _deocde_to_paths(answer_data)
+#        dir_name = "/".join(paths)
+#        tg_path = [x.split("-")[0] for x in paths[1:]]
+#
+#        buttons = _get_listdir(dir_name, tg_path)
+#
+#        row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in buttons)
+#        keyboard_markup.add(*row_btns)
+#        await bot.edit_message_reply_markup(
+#            query.from_user.id,
+#            message_id=query.message.message_id,
+#            # "Выбери тему",
+#            reply_markup=keyboard_markup,
+#        )
 
-        row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in buttons)
-        keyboard_markup.add(*row_btns)
-        await bot.edit_message_reply_markup(
-            query.from_user.id,
-            message_id=query.message.message_id,
-            # "Выбери тему",
-            reply_markup=keyboard_markup,
-        )
+#    elif _tpath[0] == "file":
+#        paths = _deocde_to_paths(answer_data)
+#        file_name = "/".join(paths)
 
-    elif _tpath[0] == "file":
-        paths = _deocde_to_paths(answer_data)
-        file_name = "/".join(paths)
+#        dir_name = "/".join(paths[:-1])
+#        tg_path = [x.split("-")[0] for x in paths[1:-1]]
 
-        dir_name = "/".join(paths[:-1])
-        tg_path = [x.split("-")[0] for x in paths[1:-1]]
+#        buttons = _get_listdir(dir_name, tg_path)
 
-        buttons = _get_listdir(dir_name, tg_path)
+#        row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in buttons)
 
-        row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in buttons)
-
-        keyboard_markup.add(*row_btns)
-
-        fl = InputFile(file_name)
-        await bot.send_document(
-            query.from_user.id,
-            document=fl,
-            reply_markup=keyboard_markup,
-        )
+#        keyboard_markup.add(*row_btns)
+#       fl = InputFile(file_name)
+#        await bot.send_document(
+#            query.from_user.id,
+#            document=fl,
+#            reply_markup=keyboard_markup,
+#        )
 
 
 def _get_listdir(path: str, tg_path: list) -> list:
